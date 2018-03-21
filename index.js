9ef13f0360a7c99e888cc3183121060e9b22b336
Jordan Punsalan <a00861859@se14l0121-sp12.ad.bcit.ca>
2018-03-20 21:53:31 -0700

    fix bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT; //|| 10000;
+const port = process.env.PORT;
 const server = require("http").Server();
 
 var io = require("socket.io") (server);
@@ -45,7 +45,6 @@ io.on("connection", function(socket){
         }
         io.emit("result", msg);
         
-  
     })
     
     socket.on("qsubmit", function(data){
